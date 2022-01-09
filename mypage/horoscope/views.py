0a835bb82286a89866b85e9a864286704c014bbe
https://apapishev@bitbucket.org <apapishev@mineradio.ru>
2022-01-09 22:21:26 +0700

    end horoskope

--- mypage/horoscope/views.py
+++ mypage/horoscope/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-list_zodiak = {
-    'aries': "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).",
-    'taurus': "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).",
-    'gemini': "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).",
-    'cancer': "Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).",
-    'leo': "Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).",
-    'virgo': "Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).",
-    'libra': "Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).",
-    'scorpio': "Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).",
-    'sagittarius': "Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).",
-    'capricorn': "Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).",
-    'aquarius': "Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).",
-    'pisces': "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)."
+list_zodiac = {
+    'aries': 'Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).',
+    'taurus': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
+    'gemini': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
+    'cancer': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
+    'leo': 'Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).',
+    'virgo': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
+    'libra': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
+    'scorpio': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
+    'sagittarius': 'Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).',
+    'capricorn': 'Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).',
+    'aquarius': 'Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).',
+    'pisces': 'Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).'
 }
 
 sign_dates = (
@@ -39,26 +39,30 @@ elements = {
 }
 
 def index(request):
-    zodiacs = list(list_zodiak.keys())
-    li_zodiac = ''
-    for _ in zodiacs:
-        redirect_path = reverse("horoscope-name", args=(_, ))
-        li_zodiac += f"<li> <a href = '{redirect_path}'>{_.title()} </a></li>"
-    responce_url = f"""
-    <ul>
-    {li_zodiac}
-    </ul>
-    """
-    return HttpResponse(responce_url)
+    zodiacs = list(list_zodiac.keys())
+    # li_zodiac += f"<li> <a href = '{redirect_path}'>{_.title()} </a></li>"
+    context = {
+       'zodiacs':zodiacs,
+        'list_zodiac': list_zodiac
+    }
+    return render(request, 'horoscope/index.html', context=context)
 
 def zodiac_sign_get_info(request, zodiac_sign:str):
-    return render(request, 'horoscope/info_zodiak.html')
+    description = list_zodiac.get(zodiac_sign)
+    zodiacs = list(list_zodiac.keys())
+    data = {
+        'description_zodiac': description,
+        'sign': zodiac_sign,
+        'sign_name': description.split()[0],
+        'zodiacs': zodiacs
+    }
+    return render(request, 'horoscope/info_zodiak.html', context=data)
 
 def zodiac_sign_get_info_num(request, zodiac_sign:int):
     if zodiac_sign > 12:
         return HttpResponseNotFound(f"Нет такого знака зодиака")
     else:
-        zodiak = list(list_zodiak.keys())[zodiac_sign-1]
+        zodiak = list(list_zodiac.keys())[zodiac_sign - 1]
         redirect_url = reverse("horoscope-name", args=(zodiak, ))
         return HttpResponseRedirect(redirect_url)
 
--- mypage/mypage/urls.py
+++ mypage/mypage/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('horoscope/', views_horoscope.index),
+    path('horoscope/', views_horoscope.index, name="horoscope-index"),
     path('horoscope/types/', views_horoscope.types_zodiac),
     path('horoscope/types/<str:type>', views_horoscope.types_element, name="horoscope-types"),
     path('horoscope/<int:month>/<int:day>', views_horoscope.get_about_from_date, name="horoscope-date"),
@@ -28,6 +28,6 @@ urlpatterns = [
     path('horoscope/<int:zodiac_sign>/', views_horoscope.zodiac_sign_get_info_num, name="horoscope-number"),
     path('horoscope/<str:zodiac_sign>', views_horoscope.zodiac_sign_get_info, name="horoscope-name"),
     path('horoscope/<str:zodiac_sign>/', views_horoscope.zodiac_sign_get_info, name="horoscope-name"),
-    path('todo_week/', include('week_days.urls')),
+    path('todo_week/', include('week_days.urls'), name="todo_week-start"),
     path('calculate_geometry/', include('geometry.urls')),
 ]
